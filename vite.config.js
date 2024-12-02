3c959b7916ba340e42460023979e22a5977bc183
Luis David Fajardo <luisda038@outlook.com>
2024-12-01 22:39:06 -0500

    ioioi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     host: true, // Escucha en todas las interfaces de red (0.0.0.0)
